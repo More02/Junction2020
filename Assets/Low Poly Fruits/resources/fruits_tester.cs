ca7ec839563291b33203ac66ae6b87a6eaa74590
Marina <mbakanova2016@yandex.ru>
2020-11-08 03:33:35 +0300

    added tables

--- Assets/Low Poly Fruits/resources/fruits_tester.cs
+++ Assets/Low Poly Fruits/resources/fruits_tester.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class fruits_tester : MonoBehaviour {
-
-	public GameObject[] fruits;
+    // int k = 0;
+    public float timeToFill = 1.0f;
+    public GameObject[] fruits;
 	// Use this for initialization
 	void Start () 
 	{
@@ -14,8 +16,62 @@ public class fruits_tester : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		
-		fruits[0].transform.RotateAround (Vector3.up, Time.deltaTime);
+        System.Random rnd = new System.Random();
+        int index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт1");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт1");
+
+        index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт2");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт2");
+
+        index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт3");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт3");
+
+        index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт4");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт4");
+
+        index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт5");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт5");
+
+        index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт6");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт6");
+        //    if (k<10)
+        //{
+        //    StartCoroutine("OnDoneAnimation");
+        //    k++;
+        //}
+
+
+        fruits[0].transform.RotateAround (Vector3.up, Time.deltaTime);
 		fruits[1].transform.RotateAround (Vector3.up, -Time.deltaTime);
 		fruits[2].transform.RotateAround (Vector3.up, Time.deltaTime);
 		fruits[3].transform.RotateAround (Vector3.up, -Time.deltaTime);
@@ -27,4 +83,23 @@ public class fruits_tester : MonoBehaviour {
 		fruits[9].transform.RotateAround (Vector3.up, -Time.deltaTime);
 	
 	}
+    IEnumerator OnDoneAnimation()
+    {
+        float startTime = Time.time;
+        float overTime = startTime + timeToFill;
+
+        while (Time.time < overTime) {
+            yield return null;
+        }
+
+            // yield return new WaitForSeconds(10);
+        System.Random rnd = new System.Random();
+        int index = rnd.Next(0, 10);
+        Debug.Log(index);
+        fruits[index].SetActive(true);
+        Debug.Log("появился фрукт");
+        fruits[index].transform.RotateAround(Vector3.up, Time.deltaTime);
+        fruits[index].SetActive(false);
+        Debug.Log("исчез фрукт");
+    }
 }
