71e7e9841563b0dc5ca21cb3b4c969d2478d7666
Marina <mbakanova2016@yandex.ru>
2020-11-08 20:44:04 +0300

    v1.1 with teleport fixes

---
+++ Assets/Prefabs/TeleportToPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToPosition : MonoBehaviour
+{
+  public void MovePlayer()
+    {
+        Player.SetPosition(transform.position);
+    }
+}
